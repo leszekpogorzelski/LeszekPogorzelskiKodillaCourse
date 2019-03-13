20fc3504118a2593489bb62140590d7e30dfcdb1
Leszek <leszekpogorzelski1@gmail.com>
2019-03-13 11:11:40 +0100

    Third commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.Calculator;
+
+public class Calculator {
+    int x;
+    int y;
+    public Calculator(int x, int y){
+        this.x =x;
+        this.y =y;
+    }
+
+    public int add(){
+        return x+y;
+    }
+
+    public int subtract(){
+        return x-y;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.Calculator.Calculator;
 public class TestingMain {
     public static void main (String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+
+            //Test SimpleUser.
+                SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+                String result = simpleUser.getUsername();
+                        if(result.equals("theForumUser")) {
+                            System.out.println("Test ok!");
+                        }
+                        else {
+                            System.out.println("Error!");
+                        }
+            //Test kalkulatora.
+            Calculator calculator = new Calculator(5,3);
+                    int wynik1 = calculator.add();
+                        int wynik2 = calculator.subtract();
+                        if(wynik1 == 8 && wynik2 == 2) {
+                            System.out.println("Test kalkulatora ok!");
+                        }
+                        else {
+                            System.out.println("Error kalkulatora!");
+                        }
+
+
     }
 }
